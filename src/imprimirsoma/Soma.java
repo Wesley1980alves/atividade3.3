1d95bd19d94016342414b2a87e8fcd5e4c3ae44a
wesleyalves <110191958+Wesley1980alves@users.noreply.github.com>
2022-10-02 20:57:09 -0300

    atividade 3.3
    Aprendendo em Java
    soma valores

---
+++ src/imprimirsoma/Soma.java
@@ -0,0 +1,17 @@
+package imprimirsoma;
+
+public class Soma {
+
+	public static void main(String[] args) {
+		int valor;
+		int soma=0;
+		int cont;
+		for(cont=10;cont<=20;cont++) {
+			soma=soma+cont;
+		}
+			System.out.println("O valor da soma dos numeros entre 10 e 20 e: "+soma);
+		
+
+	}
+
+}
